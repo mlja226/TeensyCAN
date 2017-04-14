6cac7fca60b58f38cba40801490be35ada87c220
mlja226 <mlja226@uky.edu>
2017-04-13 21:36:54 -0400

    Setup for use with PlatformIO

---
+++ src/TeensyNode.h
@@ -0,0 +1,35 @@
+#ifndef TEENSY_NODE_H
+#define TEENSY_NODE_H
+
+#include <Arduino.h>
+#include <FlexCAN.h>
+#include <kinetis_flexcan.h>
+#include "CANMessage.h"
+
+
+class TeensyNode {
+private:
+  FlexCAN CANBus;
+
+public:
+  TeensyNode(uint32_t baudrate){
+    this->CANBus = FlexCAN(baudrate);
+  }
+  int read(CANMessage &message){
+    CAN_message_t msg;
+    int result = this->CANBus.read(msg);
+    message.translateFromFlexCAN(msg);
+    return result;
+  }
+  int write(CANMessage message){
+    CAN_message_t msg;
+    message.translateToFlexCAN(msg);
+    int result = this->CANBus.write(msg);
+    return result;
+  }
+
+  
+
+};
+
+#endif
---
+++ src/canWrite.ino
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+
+#include <FlexCAN.h>
+#include <kinetis_flexcan.h>
+
+FlexCAN CANbus(500000);
+
+void setup()
+{
+  CANbus.begin();
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+
+  digitalWrite(13, HIGH);
+  CAN_message_t myMessage;
+  myMessage.id = 0x711;
+  myMessage.len = 8;
+  myMessage.buf[0] = '0';
+  myMessage.buf[1] = '1';
+  myMessage.buf[2] = '2';
+  myMessage.buf[3] = '3';
+  myMessage.buf[4] = '0';
+  myMessage.buf[5] = '0';
+  myMessage.buf[6] = '0';
+  myMessage.buf[7] = '0';
+  myMessage.timeout = 0;
+  int start = millis();
+
+  int ret = CANbus.write(myMessage);
+  int end = millis();
+  //Serial.println(end - start);
+  if(ret){
+    Serial.println(ret);
+  }
+  delay(100);
+}
