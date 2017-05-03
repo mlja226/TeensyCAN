13834fdae3f3c0eb7b7fc737ecdce73c6ddad156
mlja226 <mlja226@uky.edu>
2017-05-03 13:24:23 -0400

    Added comments and fixed indentation in BatteryNode

--- BatteryNode/src/CANMessage.h
+++ BatteryNode/src/CANMessage.h
@@ -13,16 +13,17 @@ public:
 
   uint32_t getMessageID();
   void setMessageID(uint32_t id);
-
+  //Functions for translation to/from FlexCAN and our CAN message format
   void translateFromFlexCAN(CAN_message_t from);
   void translateToFlexCAN(CAN_message_t &from);
-
+  //Functions tot read signed and unsigned integers
   uint64_t readUnsignedInt(uint8_t start, uint8_t end);
   int64_t readSignedInt(uint8_t start, uint8_t end);
-
+  //Functions to store signed and unsigned integers
   int storeUnsignedInt(uint64_t num, uint8_t start, uint8_t end);
   int storeSignedInt(int64_t num, uint8_t start, uint8_t end);
 
+  //Functions to read and store boolean values
   int storeBool(bool val ,uint8_t position);
   bool readBool(uint8_t position);
 };
--- BatteryNode/src/VoltageCellFilter.h
+++ BatteryNode/src/VoltageCellFilter.h
@@ -14,42 +14,42 @@
 
 class VoltageCellFilter : public TinyEKF {
 
-    public:
+public:
 
-        VoltageCellFilter()
-        {
-            // We approximate the process noise using a small constant for each value being checked, along the identity matrix
-            for (int i=0;i<Nsta;i++){
-              this->setQ(i, i, .0001);
-            }
+  VoltageCellFilter(){
+    // We approximate the process noise using a small constant for each value being checked, along the identity matrix
+    for (int i=0;i<Nsta;i++){
+      this->setQ(i, i, .0001);
+    }
 
-            // Same for measurement noise
-            for (int i=0;i<Mobs;i++){
-              this->setR(i, i, .0001);
-            }
-        }
+    // Same for measurement noise
+    for (int i=0;i<Mobs;i++){
+      this->setR(i, i, .0001);
+    }
 
-    protected:
-		/*
-		Model method needs to be implemented
-		Purpose: give an idea of of how we expect our data to behave as well as our measurements, give the update rules for our values
-		Parameters: fx is our updated values after our function is applied and F is the corresponding jacobian
-		hx is the associated model for the measurement values and H is the measurement's Jacobian
-		*/
+  }
 
-        void model(double fx[Nsta], double F[Nsta][Nsta], double hx[Mobs], double H[Mobs][Nsta])
-        {
-        			for (int i=0;i<Nsta;i++){
-        				fx[i] = this-> x[i];//TODO Make a proper function that is not simply constant
-        				F[i][i]=1;
-        			}
+protected:
+/*
+Model method needs to be implemented
+Purpose: give an idea of of how we expect our data to behave as well as our measurements, give the update rules for our values
+Parameters: fx is our updated values after our function is applied and F is the corresponding jacobian
+hx is the associated model for the measurement values and H is the measurement's Jacobian
+*/
 
-                    // Measurement function
-        			for (int i=0;i<Mobs;i++){
-        	         	hx[i] = this->x[i];
-        				   H[i][i] = 1;
-        			}
-        }
+  void model(double fx[Nsta], double F[Nsta][Nsta], double hx[Mobs], double H[Mobs][Nsta]){
+    for (int i=0;i<Nsta;i++){
+      fx[i] = this-> x[i];//TODO Make a proper function that is not simply constant
+      F[i][i]=1;
+    }
+
+    // Measurement function
+    for (int i=0;i<Mobs;i++){
+      hx[i] = this->x[i];
+      H[i][i] = 1;
+    }
+
+  }
 
 };
 #endif
